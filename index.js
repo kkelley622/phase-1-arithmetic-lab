59a1bcca5db9aaba75e357397837c71e2eda5ae1
Kevin Kelley <kkelley622@gmail.com>
2021-12-19 19:28:04 -0600

    commit tree 1c64a44c6751133aef6ada371c90d4737038acb4

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+
+let num1, num2, multiply;
+
+num1 = 31;
+
+num2 = 2;
+
+multiply = num1 * num2;
+
+let random;
+
+random = Math.floor(Math.random() * 10) + 1;
+
+let num3, num4, mod;
+
+num3 = 29;
+
+num4 = 5;
+
+mod = num3 % num4;
+
+let max;
+
+max = Math.max(2, 4, 8, 16, 20);
+
+
